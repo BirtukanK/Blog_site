da61b83c09dc589e9ecd711804cac8497c475626
BirtukanK <birtukankuma1113@gmail.com>
2024-07-15 20:32:29 +0300

    feat: add password reset function

--- blog_site/blog_site/urls.py
+++ blog_site/blog_site/urls.py
@@ -30,6 +30,12 @@ urlpatterns = [
     path('logout/', user_views.logout_view, name='logout'),
     path('logout/done/', user_views.logout_done, name='logout_done'),
     path('', include('blog.urls')),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password-reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
 ]
 
 if settings.DEBUG:
