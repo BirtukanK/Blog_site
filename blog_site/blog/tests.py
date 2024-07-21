e85482f75e872dbaf1cdd2ee00e707e3648e5ade
BirtukanK <birtukankuma1113@gmail.com>
2024-07-21 20:07:09 +0300

    test: testing functionalites

--- blog_site/blog/tests.py
+++ blog_site/blog/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your tests here.
+class HomeViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('blog-home')
+        self.user = User.objects.create_user(username='testuser', password='password123')
+        self.post = Post.objects.create(title='Test Post', content='Test Content', author=self.user)
+    
+    def test_home_view(self):
+        """Test GET request to home view"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/home.html')
+        self.assertContains(response, self.post.title)
+
+class PostListViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('blog-home')
+        self.user = User.objects.create_user(username='Newuser345', password='testing321')
+        self.post = Post.objects.create(title='Newusers post updated', content='This is testing post updated', author=self.user)
+    
+    def test_post_list_view(self):
+        """Test GET request to post list view"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/home.html')
+        self.assertContains(response, self.post.title)
--- blog_site/users/tests.py
+++ blog_site/users/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.messages import get_messages
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-# Create your tests here.
+class RegisterViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('register')
+    
+    def test_register_get(self):
+        """Test GET request to register view"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/register.html')
+    
+    def test_register_post_valid(self):
+        """Test POST request to register view with valid data"""
+        response = self.client.post(self.url, {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password1': 'password123',
+            'password2': 'password123'
+        })
+        self.assertEqual(response.status_code, 302)  # Redirect after success
+        self.assertRedirects(response, reverse('login'))
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), 'Account created! You can login now')
+    
+    def test_register_post_invalid(self):
+        """Test POST request to register view with invalid data"""
+        response = self.client.post(self.url, {
+            'username': 'Newuser',
+            'email': 'Newuser345@gmail.com',
+            'password1': 'testing321',
+            'password2': 'testing321'
+        })
+        self.assertEqual(response.status_code, 200)  # Re-render the form
+        self.assertTemplateUsed(response, 'users/register.html')
