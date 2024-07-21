6271c30bb3754d101bf1af1aece18a5b19296c63
BirtukanK <birtukankuma1113@gmail.com>
2024-07-21 20:16:27 +0300

    test: update tests

--- blog_site/blog/tests.py
+++ blog_site/blog/tests.py
@@ -30,3 +30,17 @@ class PostListViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/home.html')
         self.assertContains(response, self.post.title)
+
+class UserPostListViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='Newuser345', password='testing321')
+        self.post = Post.objects.create(title='Newusers post updated', content='This is testing post updated', author=self.user)
+        self.url = reverse('user-posts', kwargs={'username': self.user.username})
+    
+    def test_user_post_list_view(self):
+        """Test GET request to user post list view"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/user_posts.html')
+        self.assertContains(response, self.post.title)
